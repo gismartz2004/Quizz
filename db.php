7172834850a9219240b4b2782d9e40cf0d0e9fc8
MemersonSt <emerson.cajape@tecsu.edu.ec>
2025-12-06 00:49:26 -0500

    fix

--- db.php
+++ db.php
@@ -15,7 +15,7 @@ try {
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
-        PDO::ATTR_PERSISTENT => true
+        PDO::ATTR_PERSISTENT => false // Desactivado para evitar errores de transacción en Neon
     ];
 
     $pdo = new PDO($dsn, $user, $pass, $options);
